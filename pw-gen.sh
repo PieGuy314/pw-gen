b6126f5416c570695b5b3d697e98e3966caf1bcd
PieGuy314 <33484020+PieGuy314@users.noreply.github.com>
2017-11-21 11:55:47 +0000

    Update pw-gen.sh

--- pw-gen.sh
+++ pw-gen.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cat /dev/urandom | tr -dc 'a-hjkmnp-zA-HJ-NP-Z2-9' | fold -w 2 | sed -E 's/(...)(...)(...)(...)/\1-\2-\3-\4/' | head -n 1
+cat /dev/urandom | tr -dc 'a-hjkmnp-zA-HJ-NP-Z2-9' | fold -w 12 | sed -E 's/(...)(...)(...)(...)/\1-\2-\3-\4/' | head -n 1
